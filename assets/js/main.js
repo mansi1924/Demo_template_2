8a357f7d971f507208f46fb261d81e22009a5c95
Mansi2419 <mg5111417@gmail.com>
2023-02-08 15:23:02 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,50 @@ $(function () {
         nav: false,
         controlsText: ['<i class="fas fa-angle-left prev"></i>', '<i class="fas fa-angle-right next"></i>'],
     });
-
+    var slider1 = tns({
+        container: '.testimonial-active_team',
+        items: 2,
+        slideBy: 'page',
+        autoplay: false,
+        mouseDrag: true,
+        nav: false,
+        controlsText: ['<i class="fas fa-angle-left prev"></i>', '<i class="fas fa-angle-right next"></i>'],
+        responsive: {
+          420: {
+            items: 1
+          },
+        }
+    });
     //   scrollit js
     $.scrollIt();
 });
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+$(window).load(function() {
+    $('.post-module').hover(function() {
+      $(this).find('.description').stop().animate({
+        height: "toggle",
+        opacity: "toggle"
+      }, 300);
+    });
+  });
+
+
+
+
+ 
